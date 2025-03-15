e9af600a08b6858dbb2c1ae9d97ad12d75c0f3de
Tretyakov Maksim <120916702+Shutcing@users.noreply.github.com>
2025-03-15 21:52:29 +0500

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,16 +1,1 @@
-import base64
 
-def encrypt_string(s: str) -> str:
-    return base64.b64encode(s.encode('utf-8')).decode('utf-8')
-
-def decrypt_string(enc: str) -> str:
-    return base64.b64decode(enc.encode('utf-8')).decode('utf-8')
-
-if __name__ == "__main__":
-    original = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjZmNlcW1qanZ6dndoZHR0dWlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1NDQ1OTUsImV4cCI6MjA1NTEyMDU5NX0.2WnN1GVQTO1yPP3Dgkx5sI2xIEmmy6cKe2tbD2v2gzk"
-    encrypted = encrypt_string(original)
-    decrypted = decrypt_string(encrypted)
-
-    print("Оригинал:", original)
-    print("Зашифровано:", encrypted)
-    print("Расшифровано:", decrypted)
